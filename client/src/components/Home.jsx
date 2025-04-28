7b0a41096accd80fd1bd6caa08e849a89cfdd7a5
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-04-28 12:08:52 +1200

    remove everything: starting fresh

--- client/src/components/Home.jsx
+++
@@ -1,5 +0,0 @@
-const Home = () => {
-    return <h1 class="text-3xl underline">Home page!</h1>
-}
-
-export default Home
--- client/src/components/Login.jsx
+++
@@ -1,5 +0,0 @@
-const Login = () => {
-    return <h1 class="text-3xl underline">Login page!</h1>
-}
-
-export default Login
