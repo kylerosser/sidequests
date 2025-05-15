30c58ac119119b12c18cf73f5bd5fc4ca61fa37d
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-05-15 14:42:55 +1200

    Add /api prefix to all api routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import userRouter from './routes/userRouter';
 import authRouter from './routes/authRouter';
 
 const PORT = process.env.PORT || 3000;
+const API_PREFIX = '/api';
 const MONGODB_URI = process.env.MONGODB_URI
 if (MONGODB_URI == undefined) {
   throw new Error("MongoDB URI not provided");
@@ -19,9 +20,9 @@ mongoose.connect(MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB', err));
 
-app.use('/quest', questRouter);
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
+app.use(`${API_PREFIX}/quest`, questRouter);
+app.use(`${API_PREFIX}/user`, userRouter);
+app.use(`${API_PREFIX}/auth`, authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ success: false, message: "Resource not found" });
