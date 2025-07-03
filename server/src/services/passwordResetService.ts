efdc036972aa89ef5b4686c0b7afc041e69b47a2
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-07-03 18:29:43 +1200

    Add resetPasswordWithToken method to passwordResetService

--- server/src/services/passwordResetService.ts
+++ server/src/services/passwordResetService.ts
@@ -3,6 +3,8 @@ import { Resend } from 'resend';
 
 import { Types } from 'mongoose';
 
+import { hashPassword } from '../utils/passwordUtils'
+
 import PasswordReset from '../models/passwordResetModel';
 import User from '../models/userModel';
 
@@ -14,7 +16,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const passwordResetService = {
     sendResetEmail: async (id: Types.ObjectId, email: string) => {
-
+        // todo safely convert email to userid
         const newPasswordResetToken = crypto.randomBytes(32).toString('hex');
         const newPasswordReset = new PasswordReset({
             user: id,
@@ -30,4 +32,19 @@ export const passwordResetService = {
             html: renderEmailTemplate(passwordResetEmailTemplate, { "RESET_URL": `https://sidequests.nz/reset-password?token=${newPasswordResetToken}`}),
         });
     },
+    resetPasswordWithToken: async (token: string, newPassword:string): Promise<boolean> => {
+        const foundPasswordReset = await PasswordReset.findOne({ token: token })
+        if (!foundPasswordReset) return false;
+        if (foundPasswordReset.expiresAt < new Date()) return false; // TTL index should delete the document once it is expired, but there is a delay hence this check
+        const user = await User.findByIdAndUpdate(
+            foundPasswordReset.user,
+            { 
+                lastPasswordReset: new Date(), 
+                passwordHash: hashPassword(newPassword)
+            }
+        );
+        await foundPasswordReset.deleteOne({ _id: foundPasswordReset._id })
+        if (!user) return false;
+        return true;
+    }
 }
