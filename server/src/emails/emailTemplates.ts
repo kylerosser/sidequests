1311a99746e5fcd1914b65702733eccda7e6f650
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-07-03 17:09:47 +1200

    Add a password reset email template

--- server/src/emails/emailTemplates.ts
+++ server/src/emails/emailTemplates.ts
@@ -2,6 +2,7 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 export const verificationEmailTemplate = readFileSync(path.join(__dirname, '../emails/verificationEmailTemplate.html'), 'utf-8')
+export const passwordResetEmailTemplate = readFileSync(path.join(__dirname, '../emails/passwordResetEmailTemplate.html'), 'utf-8')
 
 export const renderEmailTemplate = (template: string, values: Record<string, string>) => {
     // Replaces any template values e.g. {{VERIFY_URL}} with a concrete value provided
