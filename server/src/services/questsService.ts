542221a6658508dbfe0de8482247bee7c39dd82d
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-07-15 20:35:45 +0800

    Fix quest fetch creator population

--- server/src/services/questsService.ts
+++ server/src/services/questsService.ts
@@ -17,11 +17,10 @@ export const questsService = {
             query.moderatorApproved = true
         };
 
-        const foundQuests = await Quest.find(query).populate('creator', 'username')
-
+        const foundQuests = await Quest.find(query).populate('creator', {username: 1})
         return foundQuests;
     },
     findQuestById: async (id: string | Types.ObjectId) => {
-        return await Quest.findById(id);
+        return await Quest.findById(id).populate('creator', {username: 1});
     }
 }
