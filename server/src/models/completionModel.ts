2c5d85e58cbdacdc249c24ddd93da039e2029f35
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-07-19 20:20:56 +1200

    Add checkListIndex field to completion model

--- server/src/models/completionModel.ts
+++ server/src/models/completionModel.ts
@@ -4,6 +4,7 @@ interface ICompletion extends Document {
   comment: string;
   completer: Types.ObjectId;
   completedQuest: Types.ObjectId;
+  checkListIndex: number;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -13,6 +14,7 @@ const CompletionSchema = new Schema<ICompletion>(
     comment: { type: String, required: true},
     completer: { type: Schema.Types.ObjectId, ref: "User", required: true },
     completedQuest: { type: Schema.Types.ObjectId, ref: "Quest", required: true },
+    checkListIndex: { type: Number, required: true}
   },
   {
     timestamps: true // adds createdAt and updatedAt Date fields
