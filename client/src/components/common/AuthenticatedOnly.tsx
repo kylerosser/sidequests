13ba429d84becf005e9f10a1c86ad740601b72c0
kylerosser <39887097+kylerosser@users.noreply.github.com>
2026-01-04 20:51:51 +1300

    Add loading check to AuthenticatedOnly.tsx

--- client/src/components/common/AuthenticatedOnly.tsx
+++ client/src/components/common/AuthenticatedOnly.tsx
@@ -3,13 +3,14 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from '../../auth/useAuth'
 
 export const AuthenticatedOnly = () => {
-    const {user} = useAuth();
+    const {user, loading} = useAuth();
     const navigate = useNavigate();
 
     useEffect(() => {
+        if (loading) return;
         if (!user) {
             navigate('/login');
         }
-    }, [user])
+    }, [user, loading, navigate])
     return null
 }
